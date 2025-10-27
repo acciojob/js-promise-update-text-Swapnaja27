3994c8504f489ee9541c0ccd66ffcdb3ef554d7c
Swapnaja27 <142212367+Swapnaja27@users.noreply.github.com>
2025-10-27 11:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let output=document.getElementById("output");
+
+let string=new Promise((res,rej)=>{
+	setTimeout(()=>{
+		res("Hello, world!");
+	},1000)
+});
+
+string.then((msg)=>{
+	output.textContent=msg;
+})
